32e6140dc2ca3030f9d3ea1f67754276fc018bdb
dreamytang <dreamytang@gmail.com>
2018-07-29 22:52:40 +0800

    Update Ranking.php
    1. Change the format of data file from xml into txt;
    2. Change the parse method of data file;
    3. Add the debug information: memory consumption and execution time

--- Ranking.php
+++ Ranking.php
@@ -1,38 +1,61 @@
 <?php
- 
+//Record start time
+$start = microtime(true);
+
 //Configuration
-$filename = "pubmed_result.xml"; //File exported from Pubmed
+$filename = "pubmed_result.txt"; //File exported from Pubmed
 $listnum = 10; //Top $listnum authors ordered by frequency
 
-//Processing
-$author_array = array();
-$doc = new DOMDocument();
-$doc->load($filename);
-$paper_nodes = $doc->getElementsByTagName("PubmedArticle");
-if ($paper_nodes->length !== 0){
-    for ($i = 0; $i < $paper_nodes->length; $i++){
-        $paper_node = $paper_nodes->item($i);
-        $author_nodes = $paper_node->getElementsByTagName("Author");
-        if ($author_nodes->length !== 0){
-            for ($ii = 0; $ii < $author_nodes->length; $ii++){
-                $author_node = $author_nodes->item($ii);
-                $lastname = $author_node->getElementsByTagName("LastName")->item(0)->nodeValue;
-                $firstname = $author_node->getElementsByTagName("ForeName")->item(0)->nodeValue;
-                array_push($author_array, $firstname . " " . $lastname);
-            }
+//Function
+function ReadMedline($file) {
+    $author_array = array();
+    $filestream = fopen($file, 'r');
+
+    while(!feof($filestream)) {
+        $author_name = trim(fgets($filestream));
+        if (!empty($author_name) && strpos($author_name, "FAU - ") !== false){
+            array_push($author_array, str_replace("FAU - ", "", $author_name));
         }
     }
+    fclose($filestream);
+    return $author_array;
 }
-$author_array_ranking = array_count_values($author_array);
-arsort($author_array_ranking);
+
+//Processing
+$authors_array = ReadMedline($filename);
+$authors_array_ranking = array_count_values($authors_array);
+arsort($authors_array_ranking);
 
 //Show Ranking
-if (count($author_array_ranking) > 0 && count($author_array_ranking) < $listnum){$listnum = count($author_array_ranking);}
+if (count($authors_array_ranking) > 0 && count($authors_array_ranking) < $listnum){$listnum = count($authors_array_ranking);}
 $for_number = 0;
-echo "<strong>Author Ranking</strong><br />";
-foreach ($author_array_ranking as $key=>$value){
+echo "<strong>Author Ranking</strong><br /><br />";
+foreach ($authors_array_ranking as $key=>$value){
     echo "<strong>Author Name:</strong> " . $key . " <strong>Frequency:</strong> " . $value . "<br />";
     $for_number++;
     if($for_number == $listnum){break;}
 }
+
+//Record end time and calculate processing time
+$end = microtime(true);
+$delta = $end - $start;
+
+//Calculate memory consumption
+function formatBytes($bytes, $precision = 2) {
+    $units = array('b', 'kb', 'mb', 'gb', 'tb');
+
+    $bytes = max($bytes, 0);
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+    $pow = min($pow, count($units) - 1);
+
+    $bytes /= (1 << (10 * $pow));
+
+    return round($bytes, $precision) . ' ' . $units[$pow];
+}
+
+echo "<br />Memory consumption: " . formatBytes(memory_get_peak_usage());
+
+//Output processing time
+echo "<br />Execution time: " . $delta . PHP_EOL . " s";
+
 ?>
